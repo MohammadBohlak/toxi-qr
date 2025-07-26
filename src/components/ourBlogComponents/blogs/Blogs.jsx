0a2e1f239b6aec50d63250c7a4e60277bd22a895
MohammadBohlak <mohammad.bohlak5@gmail.com>
2025-07-26 21:30:39 +0200

    ..

--- src/components/ourBlogComponents/blogs/Blogs.jsx
+++ src/components/ourBlogComponents/blogs/Blogs.jsx
@@ -3,13 +3,10 @@ import { SmallText, SubTitle, Text } from "../../common/texts";
 import { Link } from "react-router-dom";
 import { BlogCard } from "./blogs.styles";
 import blog from "../../../assets/images/blog.jpg";
-import blog2 from "../../../assets/images/blog2.jpg";
-import blog3 from "../../../assets/images/blog3.jpg";
 const Blogs = ({ blogs, isOneBlog = false }) => {
-  console.log(blogs);
   return (
     <>
-      {[blogs[0]].map((item, idx) => {
+      {blogs.map((item, idx) => {
         return (
           <BlogCard $shadow={!isOneBlog} key={idx}>
             <SubTitle className="mb-4">
@@ -25,87 +22,7 @@ const Blogs = ({ blogs, isOneBlog = false }) => {
                 objectFit: "contain",
                 backgroundColor: `${!item.image ? "white" : "#212832"}`,
               }}
-              src={!item.image ? item.image : blog}
-              alt={item.title}
-            />
-            <Card.Body className="p-0 pt-3">
-              <Text $bold={true} className="mb-2">
-                {item.title}
-              </Text>
-              <SmallText className="mb-2 text-muted text-uppercase ">
-                {item.date}
-              </SmallText>
-              <SmallText className="mb-2">
-                {isOneBlog
-                  ? `${item.description}`
-                  : `${item.short_description}`}
-              </SmallText>
-              <SmallText>
-                {!isOneBlog && (
-                  <Link to={`/blog/${item.id}`}>Read the full article</Link>
-                )}
-              </SmallText>
-            </Card.Body>
-          </BlogCard>
-        );
-      })}
-      {[blogs[0]].map((item, idx) => {
-        return (
-          <BlogCard $shadow={!isOneBlog} key={idx}>
-            <SubTitle className="mb-4">
-              {idx + 1}. In the{" "}
-              {/* {item.country.charAt(0).toUpperCase() +
-                item.country.slice(1).toLowerCase()} */}
-              {item.country}
-            </SubTitle>
-            <Card.Img
-              style={{
-                maxHeight: "180px",
-                width: "100%",
-                objectFit: "contain",
-                backgroundColor: `${!item.image ? "white" : "#212832"}`,
-              }}
-              src={blog2}
-              alt={item.title}
-            />
-            <Card.Body className="p-0 pt-3">
-              <Text $bold={true} className="mb-2">
-                {item.title}
-              </Text>
-              <SmallText className="mb-2 text-muted text-uppercase ">
-                {item.date}
-              </SmallText>
-              <SmallText className="mb-2">
-                {isOneBlog
-                  ? `${item.description}`
-                  : `${item.short_description}`}
-              </SmallText>
-              <SmallText>
-                {!isOneBlog && (
-                  <Link to={`/blog/${item.id}`}>Read the full article</Link>
-                )}
-              </SmallText>
-            </Card.Body>
-          </BlogCard>
-        );
-      })}
-      {[blogs[0]].map((item, idx) => {
-        return (
-          <BlogCard $shadow={!isOneBlog} key={idx}>
-            <SubTitle className="mb-4">
-              {idx + 1}. In the{" "}
-              {/* {item.country.charAt(0).toUpperCase() +
-                item.country.slice(1).toLowerCase()} */}
-              {item.country}
-            </SubTitle>
-            <Card.Img
-              style={{
-                maxHeight: "180px",
-                width: "100%",
-                objectFit: "contain",
-                backgroundColor: `${!item.image ? "white" : "#c5a115"}`,
-              }}
-              src={blog3}
+              src={item.image ? item.image : blog}
               alt={item.title}
             />
             <Card.Body className="p-0 pt-3">
