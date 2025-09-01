abc03eae93ce95e0ea49d1602dea76d554f31dce
MohammadBohlak <mohammad.bohlak5@gmail.com>
2025-09-01 17:54:43 +0300

    simple edit

--- src/pages/ourBlog/BlogList.jsx
+++ src/pages/ourBlog/BlogList.jsx
@@ -3,13 +3,19 @@ import { useOutletContext } from "react-router-dom";
 import Blogs from "../../components/ourBlogComponents/blogs/Blogs";
 import { SubTitle } from "../../components/common/texts";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 
 export default function BlogList() {
+  const showLoader = useSelector((state) => state.loader.isLoading);
+
   const { blogs } = useOutletContext();
   const { t } = useTranslation("ourBlog");
-  return blogs.length > 0 ? (
-    <Blogs blogs={blogs} />
-  ) : (
-    <SubTitle> {t("searchNotFound")}</SubTitle>
+  return (
+    !showLoader &&
+    (blogs.length > 0 ? (
+      <Blogs blogs={blogs} />
+    ) : (
+      <SubTitle> {t("searchNotFound")}</SubTitle>
+    ))
   );
 }
